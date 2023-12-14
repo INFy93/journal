1de73470cc8466012aba7f3bf5e04ace96943b77
INFy93 <infy93@gmail.com>
2023-12-14 11:36:26 +0300

    add request for user

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -11,7 +11,7 @@ class UserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,19 @@ class UserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required'],
+            'email' => ['required'],
+            'phone' => ['required'],
+            'password' => ['nullable', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Необходимо ввести имя пользователя',
+            'email.required' => 'Необходимо ввести e-mail',
+            'phone.required' => 'Необходимо ввести телефон',
         ];
     }
 }

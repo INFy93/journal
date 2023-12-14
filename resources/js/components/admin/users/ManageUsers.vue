98d3845923af1fed8665ffa1008ebd255a7b1a5c
INFy93 <infy93@gmail.com>
2023-12-14 10:31:12 +0300

    add icons for edit / block user

--- resources/js/components/admin/users/ManageUsers.vue
+++ resources/js/components/admin/users/ManageUsers.vue
@@ -17,7 +17,7 @@
                     <tbody class="divide-y divide-gray-100">
                         <tr class="bg-white" v-for="user in users" :key="user.id">
                             <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
-                                {{ user.name }}
+                                <span class="font-bold">{{ user.name }}</span>
                             </td>
                             <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
                                 {{ user.phone }}
@@ -28,7 +28,21 @@
                             <td class="p-3 text-sm text-gray-700 whitespace-nowrap">{{ user.isAdmin ? 'Администратор' :
                                 'Пользователь' }}</td>
                             <td class="p-3 text-sm text-gray-700 whitespace-nowrap">давно</td>
-                            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">Удалить / Блок</td>
+                            <td class="p-3 text-sm text-gray-700 whitespace-nowrap">
+                                <div class="flex space-x-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                                    </svg>
+
+                                </div>
+                            </td>
                         </tr>
                     </tbody>
                 </table>

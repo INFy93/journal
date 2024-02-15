3b006b0abce9c4f550506ca959d22a650358517c
INFy93 <infy93@gmail.com>
2024-02-15 11:53:29 +0300

    add city edit moonshine resource

--- app/MoonShine/Resources/CityResource.php
+++ app/MoonShine/Resources/CityResource.php
@@ -7,6 +7,7 @@ namespace App\MoonShine\Resources;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\City;
 
+use MoonShine\Fields\Text;
 use MoonShine\Resources\ModelResource;
 use MoonShine\Decorations\Block;
 use MoonShine\Fields\ID;
@@ -20,11 +21,20 @@ class CityResource extends ModelResource
 
     protected string $title = 'Нас. пункты';
 
+    protected string $sortDirection = 'ASC'; // Тип сортировки по умолчанию
+
+    protected bool $createInModal = true;
+
+    protected bool $editInModal = true;
+
+    protected bool $detailInModal = true;
+
     public function fields(): array
     {
         return [
             Block::make([
                 ID::make()->sortable(),
+                Text::make("Нас. пункт", 'city_name')
             ]),
         ];
     }
@@ -32,5 +42,10 @@ class CityResource extends ModelResource
     public function rules(Model $item): array
     {
         return [];
+    }
+
+    public function redirectAfterSave(): string
+    {
+        return '/admin/resource/city-resource/index-page';
     }
 }
